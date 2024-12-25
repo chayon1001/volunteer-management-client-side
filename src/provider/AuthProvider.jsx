46716afa08158a408efd19328360325f4c1e7cb4
chayon1001 <baust.cse.200201001@gmail.com>
2024-12-26 01:26:52 +0600

    added the jwt token create and delete for login user

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,77 +1,103 @@
 import React, { createContext, useEffect, useState } from 'react';
 import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
+    createUserWithEmailAndPassword,
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
 } from "firebase/auth";
 import { Toaster } from 'react-hot-toast';
 import app from '../components/firebase/firebase.config';
+import axios from 'axios';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  // Create User
-  const createUser = async (email, password) => {
-    setLoading(true);
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      await signOut(auth); 
-    } finally {
-      setLoading(false);
-    }
-  };
+    // Create User
+    const createUser = async (email, password) => {
+        setLoading(true);
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            await signOut(auth);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-  // Login User
-  const loginUser = (email, password) => {
-    setLoading(true);
-    return signInWithEmailAndPassword(auth, email, password);
-  };
+    // Login User
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
 
-  // Google Login
-  const logInWithGoogle = () => {
-    return signInWithPopup(auth, googleProvider);
-  };
+    // Google Login
+    const logInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    };
 
-  // Logout User
-  const logOut = () => {
-    setLoading(true);
-    return signOut(auth);
-  };
+    // Logout User
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
 
-  // Persist User on Reload
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser); 
-      setLoading(false);
-    });
-    return () => unsubscribe();
-  }, []);
+    // Persist User on Reload
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
 
-  const authInfo = {
-    user,
-    setUser,
-    createUser,
-    loginUser,
-    logOut,
-    loading,
-    logInWithGoogle,
-  };
+            console.log('state captured', currentUser?.email)
 
-  return (
-    <AuthContext.Provider value={authInfo}>
-      <Toaster />
-      {!loading && children}
-    </AuthContext.Provider>
-  );
+            if (currentUser?.email) {
+                const user = { email: currentUser.email }
+
+                axios.post('http://localhost:5000/jwt', user, { withCredentials: true })
+                    .then(res => {
+                        console.log(res.data)})
+                        setLoading(false);
+            }
+            else {
+                axios.post('http://localhost:5000/logout', {}, {
+                    withCredentials: true
+                })
+                    .then(res => {
+                        console.log('logout', res.data)
+                    })
+                setLoading(false);
+
+            }
+
+
+
+        });
+
+
+        return () => unsubscribe();
+    }, []);
+
+    const authInfo = {
+        user,
+        setUser,
+        createUser,
+        loginUser,
+        logOut,
+        loading,
+        logInWithGoogle,
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            <Toaster />
+            {!loading && children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
