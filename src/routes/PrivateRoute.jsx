fcf60137d9d4cb1bbc67948b88160e7d7a35b1c9
chayon1001 <baust.cse.200201001@gmail.com>
2024-12-26 11:25:56 +0600

    added the private route plus loading spinner for data fetches all routes

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import React, {useContext } from 'react';
+
+import { Navigate } from 'react-router-dom';
+
+import { AuthContext } from '../provider/AuthProvider';
+import Loading from '../pages/loading/Loading';
+
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext)
+
+    if(loading){
+        return <Loading></Loading>
+    }
+    if(user && user?.email){
+       return children;
+    }
+    return <Navigate to={'/auth/login'}></Navigate>
+    
+};
+
+export default PrivateRoute;
